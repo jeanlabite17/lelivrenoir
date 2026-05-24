db05a96874c7508c428151b315d7d99f06703f36
jeanlabite17 <jean.labite17@gmail.com>
2026-05-24 09:00:46 +0200

    Update data.js

--- data.js
+++ data.js
@@ -14,7 +14,7 @@ const PROFILES = [
     city: "Paris",
     bio: "Laurent 62 ans Chantage - négociation - culpabilisation - manipulation - insiste pour avoir des photos - fantasmeur.",
     photo: "images/laurent-damarly.jpg",
-    photos: ["images/tess-laurent-damarly.jpg"],
+    photos: [],
     phone: "+33 7 49 76 59 27",
     website: "https://laurentdemarly.fr/",
     socials: {
@@ -24,27 +24,6 @@ const PROFILES = [
       github: "",
       tiktok: "",
       facebook: "https://www.facebook.com/ECLAIREURCOACH/",
-      youtube: ""
-    }
-  },
-  {
-    id: "nadia-ait-salem",
-    firstName: "Nadia",
-    lastName: "Aït-Salem",
-    profession: "Brand strategist",
-    city: "Marseille",
-    bio: "Stratégiste créative spécialisée en identité de marque et storytelling. Nadia a accompagné plus de 30 marques dans leur repositionnement avec une approche humaine et différenciante.",
-    photo: "images/nadia.jpg",
-    photos: [],
-    phone: "+33 6 98 76 54 32",
-    website: "",
-    socials: {
-      instagram: "https://instagram.com/nadiaait",
-      linkedin: "https://linkedin.com/in/nadiaait",
-      twitter: "",
-      github: "",
-      tiktok: "https://tiktok.com/@nadiaait",
-      facebook: "",
       youtube: ""
     }
   }
